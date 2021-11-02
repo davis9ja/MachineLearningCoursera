24981f141e3362fceb2294fa829ef0334745c961
davis9ja <daviso53@msu.edu>
2021-11-02 12:56:46 -0400

    complete scan for CV-evaluated best params

--- ex6_SVMs/dataset3Params.m
+++ ex6_SVMs/dataset3Params.m
@@ -23,11 +23,52 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Store results in struct, for fun.
+CV_results = struct();
 
+params = [0.01, 0.03, 0.10, 0.30, 1.00, 3.00, 10.0, 30.0];
+sz = size(params,2);
 
+C_best = 1;
+sigma_best = 1;
+error_min = 1;
 
+fprintf("Start CV parameter search on C, sigma; %d points\n", sz);
+for i = 1:sz
+	  for j = 1:sz
+      	C = params(i);
+        sigma = params(j);
+        
+        fprintf("\nTraining on C = %0.2f, sigma = %0.2f\n", C, sigma);
 
+        % Train SVM model on current params.
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        
+        CV_results.C = C;
+        CV_results.sigma = sigma;
+        CV_results.error = error;
+        
+        % If error is smaller than previous, set new best params.
+        if ( error <= error_min )
+          C_best = C;
+          sigma_best = sigma;
+          error_min = error;
+          
+        endif
+        
+        fprintf("Error = %0.8f\n", error);
+	  endfor
+endfor
 
+CV_results
+
+fprintf("Best params (C, sigma) = (%0.2f, %0.2f)", C_best, sigma_best);
+
+% Return best params.
+C = C_best;
+sigma = sigma_best;
 
 % =========================================================================
 
